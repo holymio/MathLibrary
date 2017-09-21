6e9557981e43ad71fc59525cc48ab19f0bc87949
Mio <holymolymio@gmail.com>
2017-09-21 21:17:49 +0200

    initial commit

---
+++ MathLibrary/MathLibrary.cpp
@@ -0,0 +1,42 @@
+// MathLibrary.cpp : Defines the exported functions for the DLL.
+#include "stdafx.h"
+#include <utility>
+#include <limits.h> 
+#include "MathLibrary.h"
+
+// DLL internal state variables:
+static unsigned long long previous_;  // Previous value, if any
+static unsigned long long current_;   // Current sequence value
+static unsigned index_;               // Current seq. position
+
+// Initialize a Fibonacci relation sequence
+// such that F(0) = a, F(1) = b. 
+// This function must be called before any other function.
+void fibonacci_init(const unsigned long long a, const unsigned long long b) 
+{
+	index_ = 0;
+	current_ = a;
+	previous_ = b;
+}
+
+// Produce the next value in the sequence.
+// Returns true on success, false on overflow.
+bool fibonacci_next()
+{
+	// check to see if we'd overflow result or position
+	if ((ULLONG_MAX - previous_ < current_) ||
+		(UINT_MAX == index_))
+	{
+		return false;
+	}
+
+	// Special case when index == 0, just return b value
+	if (index_ > 0)
+	{
+		// otherwise, calculate next sequence value
+		previous_ += current_;
+	}
+	std::swap(current_, previous_);
+	++index_;
+	return true;
+}
